2bb3e3ad9680c22e3aecea4744c1ad6bb8e1ed95
SteveZhang <871506263@qq.com>
2025-02-13 19:21:40 +0800

    优化

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,7 @@
         <NuxtLink to="/" class="text-xl font-bold">每日诗词 AI 画图</NuxtLink>
         <div class="flex gap-4">
           <NuxtLink to="/about" class="text-gray-600 hover:text-gray-900">关于</NuxtLink>
+          <NuxtLink to="https://github.com/jay6697117/we-drawing-nuxt" class="text-gray-600 hover:text-gray-900">GitHub</NuxtLink>
         </div>
       </div>
     </nav>
@@ -17,8 +18,8 @@
       <div class="container mx-auto px-4 text-center text-gray-600">
         <p class="mb-2">© {{ new Date().getFullYear() }} 每日诗词 AI 画图</p>
         <p class="text-sm">
-          <a href="https://github.com/liruifengv/daily-poetry-image" target="_blank" class="hover:text-gray-900">
-            GitHub
+          <a href="https://github.com/liruifengv/we-drawing" target="_blank" class="hover:text-gray-900">
+            感谢liruifengv&nbsp;-&nbsp;GitHub
           </a>
         </p>
       </div>
